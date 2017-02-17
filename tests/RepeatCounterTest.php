301aa247bf1910dcb3787e744ddaaca9cb41a24a
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-02-17 09:49:04 -0800

    fail spec 2 test -test_one_anycase_match-.

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -13,6 +13,14 @@
             $result = $test_object->CountRepeats($test_input_to_match, $test_input_to_search);
             $this->assertEquals(1, $result);
         }
+        function test_one_anycase_match()
+        {
+            $test_input_to_match = "Apple";
+            $test_input_to_search = "apple";
+            $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
+            $result = $test_object->CountRepeats($test_input_to_match, $test_input_to_search);
+            $this->assertEquals(1, $result);
+        }
     }
 
  ?>
