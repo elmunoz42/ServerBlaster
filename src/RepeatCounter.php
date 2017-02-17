48dc7b5c379f74ef67f6a5680aeb037b889e71ca
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-02-17 10:50:20 -0800

    pass spec 3b test-test_multiple_matches_possessive-.

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -16,7 +16,8 @@ class RepeatCounter
         $this->count = 0;
         $input_to_find = strtolower( (string) $new_text_to_find);
         $input_to_search = strtolower( (string) $new_text_to_search);
-        $to_search_array = explode(" ", $input_to_search );
+        $input_no_possessive = implode(" ", explode("'", $input_to_search));
+        $to_search_array = explode(" ", $input_no_possessive );
 
         foreach ( $to_search_array as $word ){
             if ( $input_to_find == $word){
