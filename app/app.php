5d4de60abdce0390e58b8ef95df0d421e71996db
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-02-25 22:34:00 -0800

    check page for errors.

--- app/app.php
+++ app/app.php
@@ -3,9 +3,12 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/RepeatCounter.php";
 
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
     $app = new Silex\Application();
 
-    $app['debug'] = true;
+    $app['debug']=true;
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
@@ -13,7 +16,6 @@
 
     // ROOT form page
     $app->get("/", function() use($app) {
-
 
         return $app['twig']->render('repeat-counter-form.html.twig');
 
