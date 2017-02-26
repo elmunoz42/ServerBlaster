2e345a42301690e42f2eccff714cce8b88265e93
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-02-25 22:18:13 -0800

    pass all previous and new tests.

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -66,104 +66,119 @@
             //Assert
             $this->assertEquals($input_count , $result);
         }
-        // function test_getTextToSearch()
-        // {
-        //     //Arrange
-        //     $test_input_to_match = "apple";
-        //     $test_input_to_search = "apple";
-        //     $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
-        //     //Act
-        //     $result = $test_object->getTextToSearch();
-        //     //Assert
-        //     $this->assertEquals($test_input_to_search , $result);
-        // }
-        // function test_one_lowercase_match()
-        // {
-        //     $test_input_to_match = "apple";
-        //     $test_input_to_search = "apple";
-        //     $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
-        //     $result = $test_object->CountRepeats($test_input_to_match, $test_input_to_search);
-        //     $this->assertEquals(1 . ' match', $result);
-        // }
-        // function test_one_anycase_match()
-        // {
-        //     $test_input_to_match = "Apple";
-        //     $test_input_to_search = "apple";
-        //     $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
-        //     $result = $test_object->CountRepeats($test_input_to_match, $test_input_to_search);
-        //     $this->assertEquals(1 . ' match', $result);
-        // }
-        // function test_zero_matches()
-        // {
-        //     $test_input_to_match = "apple";
-        //     $test_input_to_search = "orange";
-        //     $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
-        //     $result = $test_object->CountRepeats($test_input_to_match, $test_input_to_search);
-        //     $this->assertEquals(0 . ' matches', $result);
-        // }
-        // function test_multiple_matches()
-        // {
-        //     $test_input_to_match = "apple";
-        //     $test_input_to_search = "The Apple logo is meaningful because it alludes to the apple in the tree of knowledge.";
-        //     $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
-        //     $result = $test_object->CountRepeats($test_input_to_match, $test_input_to_search);
-        //     $this->assertEquals(2 . ' matches', $result);
-        //
-        // }
-        // function test_multiple_matches_possessive()
-        // {
-        //     $test_input_to_match = "apple";
-        //     $test_input_to_search = "Apple's logo is meaningful because it alludes to the apple in the tree of knowledge.";
-        //     $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
-        //     $result = $test_object->CountRepeats($test_input_to_match, $test_input_to_search);
-        //     $this->assertEquals(2 . ' matches', $result);
-        //
-        // }
-        //
-        // function test_case_specific_option()
-        // {
-        //     $test_input_to_match = "apple";
-        //     $test_input_to_search = "The Apple logo is meaningful because it alludes to the apple in the tree of knowledge.";
-        //     $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
-        //     $result = $test_object->CountExactRepeats($test_input_to_match, $test_input_to_search);
-        //     $this->assertEquals(1 . ' exact match', $result);
-        //
-        // }
-        // function test_TextReplace()
-        // {
-        //     $test_input_to_match = "apple";
-        //     $test_input_to_search = "The Apple logo is meaningful because it alludes to the apple in the tree of knowledge.";
-        //     $test_input_to_replace = "orange";
-        //     $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
-        //     $result = $test_object->TextReplace($test_input_to_match, $test_input_to_search, $test_input_to_replace);
-        //     // $this->assertEquals(16, $result);
-        //     $this->assertEquals("The Apple logo is meaningful because it alludes to the orange in the tree of knowledge.", $result);
-        //
-        // }
-        //
-        // //Spec 6
-        // function test_multiple_matches_punctuation()
-        // {
-        //     $test_input_to_match = "apple";
-        //     $test_input_to_search = "Apple, banana, orange.";
-        //     $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
-        //     $result = $test_object->CountRepeats($test_input_to_match, $test_input_to_search);
-        //     $this->assertEquals(1 . ' match', $result);
-        //
-        // }
-        //
-        // //Spec 6b
-        // function test_TextReplacePonctuation()
-        // {
-        //     $test_input_to_match = "orange";
-        //     $test_input_to_search = "Apple, banana, orange.";
-        //     $test_input_to_replace = "fig";
-        //     $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
-        //     $result = $test_object->TextReplace($test_input_to_match, $test_input_to_search, $test_input_to_replace);
-        //     // $this->assertEquals(16, $result);
-        //     $this->assertEquals("Apple, banana, fig.", $result);
-        //
-        // }
+    
+        function test_one_lowercase_match()
+        {
+            //Arrange
+            $test_input_to_match = "apple";
+            $test_input_to_search = "apple";
+            $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
+            //Act
+            $result = $test_object->CountRepeats($test_input_to_match, $test_input_to_search);
+            //Assert
+            $this->assertEquals(1 . ' match', $result);
+        }
+        function test_one_anycase_match()
+        {
+            //Arrange
+            $test_input_to_match = "Apple";
+            $test_input_to_search = "apple";
+            $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
+            //Act
+            $result = $test_object->CountRepeats($test_input_to_match, $test_input_to_search);
+            //Assert
+            $this->assertEquals(1 . ' match', $result);
+        }
+        function test_zero_matches()
+        {
+            //Arrange
+            $test_input_to_match = "apple";
+            $test_input_to_search = "orange";
+            $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
+            //Act
+            $result = $test_object->CountRepeats($test_input_to_match, $test_input_to_search);
+            //Assert
+            $this->assertEquals(0 . ' matches', $result);
+        }
+        function test_multiple_matches()
+        {
+            //Arrange
+            $test_input_to_match = "apple";
+            $test_input_to_search = "The Apple logo is meaningful because it alludes to the apple in the tree of knowledge.";
+            $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
+            //Act
+            $result = $test_object->CountRepeats($test_input_to_match, $test_input_to_search);
+            //Assert
+            $this->assertEquals(2 . ' matches', $result);
+
+        }
+        function test_multiple_matches_possessive()
+        {
+            //Arrange
+            $test_input_to_match = "apple";
+            $test_input_to_search = "Apple's logo is meaningful because it alludes to the apple in the tree of knowledge.";
+            $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
+            //Act
+            $result = $test_object->CountRepeats($test_input_to_match, $test_input_to_search);
+            //Assert
+            $this->assertEquals(2 . ' matches', $result);
+
+        }
+
+        function test_case_specific_option()
+        {
+            //Arrange
+            $test_input_to_match = "apple";
+            $test_input_to_search = "The Apple logo is meaningful because it alludes to the apple in the tree of knowledge.";
+            $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
+            //Act
+            $result = $test_object->CountExactRepeats($test_input_to_match, $test_input_to_search);
+            //Assert
+            $this->assertEquals(1 . ' exact match', $result);
+
+        }
+        function test_TextReplace()
+        {
+            //Arrange
+            $test_input_to_match = "apple";
+            $test_input_to_search = "The Apple logo is meaningful because it alludes to the apple in the tree of knowledge.";
+            $test_input_to_replace = "orange";
+            $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
+            //Act
+            $result = $test_object->TextReplace($test_input_to_match, $test_input_to_search, $test_input_to_replace);
+            //Assert
+            $this->assertEquals("The Apple logo is meaningful because it alludes to the orange in the tree of knowledge.", $result);
+
+        }
+
+        //Spec 6
+        function test_multiple_matches_punctuation()
+        {
+            //Arrange
+            $test_input_to_match = "apple";
+            $test_input_to_search = "Apple, banana, orange.";
+            $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
+            //Act
+            $result = $test_object->CountRepeats($test_input_to_match, $test_input_to_search);
+            //Assert
+            $this->assertEquals(1 . ' match', $result);
+
+        }
+
+        //Spec 6b
+        function test_TextReplacePonctuation()
+        {
+            //Arrange
+            $test_input_to_match = "orange";
+            $test_input_to_search = "Apple, banana, orange.";
+            $test_input_to_replace = "fig";
+            $test_object = new RepeatCounter($test_input_to_match, $test_input_to_search);
+            //Act
+            $result = $test_object->TextReplace($test_input_to_match, $test_input_to_search, $test_input_to_replace);
+            //Assert
+            $this->assertEquals("Apple, banana, fig.", $result);
+
+        }
 
     }
 
